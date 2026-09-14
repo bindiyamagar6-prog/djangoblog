5777d2024ff8a17c99dae58551821c73a709d8a5
bindiyamagar6-prog <bindiyamagar6@gmail.com>
2026-09-14 12:30:55 +0900

    PostgreSQL Database Post Model

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, "blog/home.html", {"team":"Welcome to djangoBlog"})
+
+def about(request):
+    return render(request, "blog/about.html", {"team": "Us"})
+
+def contact(request):
+    return render(request, "blog/contact.html", {"team": "Us"})
--- config/settings.py
+++ config/settings.py
@@ -13,12 +13,10 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv()
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
@@ -78,8 +76,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
@@ -119,6 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'blog' / 'templates' / 'blog' /'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
